1b513cf8f52c19ddc9ed64ef707dd77c1037e185
mo12345671 <moamedaleh@gmail.com>
2025-08-21 22:37:05 +0100

    cancer.py

--- cancer.py
+++ cancer.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import StandardScaler
 
 
 def get_clean_data():
-  data = pd.read_csv("data/data.csv")
+  data = pd.read_csv("data.csv")
   
   data = data.drop(['Unnamed: 32', 'id'], axis=1)
   
@@ -180,3 +180,4 @@ def main():
 
 if __name__ == '__main__':
     main() 
+
